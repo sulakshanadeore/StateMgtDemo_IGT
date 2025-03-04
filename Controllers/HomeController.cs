54eb2911777dc4b4a51b31e16f505123b04cc3c6
sulakshanadeore <deoresulakshana@gmail.com>
2025-03-04 16:00:26 +0530

    Cookie and Query String in Home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,21 +10,46 @@ namespace StateMgtDemo.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            string name = "Jack";
+            HttpCookie cookie = new HttpCookie("userdata");
+            cookie.Value = name;
+            cookie.Expires = DateTime.Now.AddMinutes(5);
+            Response.Cookies.Add(cookie);
+            //Created the cookie and redirected  to Contact Page
+
+
+            return RedirectToAction("Contact");
         }
 
         public ActionResult About()
         {
+            //Pass query string using route parameters
             ViewBag.Message = "Your application description page.";
+            string name = "Jim";
+            int age = 10;
 
-            return View();
+            return RedirectToAction("ShowData", new { username =name,userage=age });
         }
 
+
+
+
+        public ActionResult ShowData(string username,int userage)
+        {
+            //Access query string route parameters
+            string uname = username;
+            int uage = userage;
+
+
+            return Content(string.Concat(uname, uage));
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+            HttpCookie co = Request.Cookies["userdata"];
+            string CookieData = co.Value;
 
-            return View();
+            return Content(CookieData);
         }
     }
 }
